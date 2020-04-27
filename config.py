a6a4cf19190a8994b234df7b18d7cd939f53ced5
crazynayan <nayan@crazyideas.co.in>
2020-04-27 14:39:55 +0530

    Google sheet connectivity

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google-cloud.json'
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or b64encode(os.urandom(24)).decode()
+    SHEET_ID = "1OnvVprC_RSlsKxgAgve-dliPGJydj--qkmvnRUfyGYM"
--- fs.py
+++ fs.py
@@ -1,10 +1,8 @@
-from config import os
+# noinspection PyPackageRequirements
+from googleapiclient.discovery import build
+
+from config import Config
 from fs_flask.user import User
-
-
-def check_env():
-    for key, value in os.environ.items():
-        print(f"{key}: {value}")
 
 
 def create_user(email: str, name: str, initial: str):
@@ -14,3 +12,15 @@ def create_user(email: str, name: str, initial: str):
     else:
         print(f"User {email} created. Your password is {password}")
     return
+
+
+def mumbai_hotels():
+    sheet = build("sheets", "v4").spreadsheets().values()
+    hotel_table = sheet.get(spreadsheetId=Config.SHEET_ID, range="Mumbai!A1:Z50").execute().get("values", list())
+    hotel_dict = {str(index): {"name": hotel, "rooms": list()} for index, hotel in enumerate(hotel_table[0])}
+    for rooms in hotel_table[1:]:
+        for index, room in enumerate(rooms):
+            if room:
+                hotel_dict[str(index)]["rooms"].append(room)
+    for _, hotel in hotel_dict.items():
+        print(hotel)
