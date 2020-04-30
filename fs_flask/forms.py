916b33ce3399fb3e477761d274082ed6f18f6dac
crazynayan <nayan@crazyideas.co.in>
2020-04-30 10:54:25 +0530

    Enhance Hotel Count reporting

--- fs_flask/forms.py
+++ fs_flask/forms.py
@@ -2,37 +2,49 @@ from operator import itemgetter
 from typing import List
 
 from flask_wtf import FlaskForm
-from wtforms import SelectField, SubmitField
+from wtforms import SelectField, SubmitField, SelectMultipleField
 
 from config import Config
 from fs_flask.hotel import Usage, Hotel
 from fs_flask.user import User
 
 
-class QueryForm(FlaskForm):
+class EventForm(FlaskForm):
     timing = SelectField("Select timing", choices=list())
-    meal = SelectField("Select meals", choices=list())
+    meals = SelectMultipleField("Select meals", choices=list())
     event = SelectField("Select event type", choices=list())
     submit = SubmitField("Query")
 
     def populate_choices(self) -> None:
         any_choice = (Config.ANY, Config.ANY)
         self.timing.choices.append(any_choice)
-        self.meal.choices.append(any_choice)
+        self.meals.choices.append(any_choice)
         self.event.choices.append(any_choice)
         self.timing.choices.extend([(timing, timing) for timing in Config.TIMINGS])
-        self.meal.choices.extend([(meal, meal) for meal in Config.MEALS])
+        self.meals.choices.extend([(meal, meal) for meal in Config.MEALS])
         self.event.choices.extend([(event, event) for event in Config.EVENTS])
 
     def get_usage(self, city: str) -> List[Usage]:
         query = Usage.objects.filter_by(city=city)
-        if self.timing.data != Config.ANY:
-            query = query.filter_by(timing=self.timing.data)
-        if self.event.data != Config.ANY:
-            query = query.filter_by(event_type=self.event.data)
-        if self.meal.data != Config.ANY:
-            query = query.filter("meals", query.ARRAY_CONTAINS, self.meal.data)
+        if self.validate_on_submit():
+            if self.timing.data != Config.ANY:
+                query = query.filter_by(timing=self.timing.data)
+            if self.event.data != Config.ANY:
+                query = query.filter_by(event_type=self.event.data)
+            if self.meals.data[0] != Config.ANY:
+                query = query.filter("meals", query.ARRAY_CONTAINS_ANY, self.meals.data)
+        else:
+            self.timing.data = Config.ANY
+            self.event.data = Config.ANY
+            self.meals.data = [Config.ANY]
         return query.get()
+
+    def get_selection(self) -> dict:
+        return {
+            "timing": self.timing.data,
+            "event": self.event.data,
+            "meals": self.meals.data
+        }
 
 
 class ProfileForm(FlaskForm):
--- fs_flask/routes.py
+++ fs_flask/routes.py
@@ -6,7 +6,7 @@ from flask_login import login_required, current_user
 
 from config import Config
 from fs_flask import fs_app
-from fs_flask.forms import QueryForm, ProfileForm
+from fs_flask.forms import EventForm, ProfileForm
 from fs_flask.hotel import Usage
 
 
@@ -17,22 +17,17 @@ def home():
     return render_template("home.html", title="Dashboard")
 
 
-@fs_app.route("/query", methods=["GET", "POST"])
+@fs_app.route("/hotel_count", methods=["GET", "POST"])
 @login_required
-def query():
-    form = QueryForm()
+def hotel_count():
+    form = EventForm()
     form.populate_choices()
-    if not form.validate_on_submit():
-        return render_template("form_template.html", title="Query", name="Dashboard", name_url=url_for("home"),
-                               form=form)
-    return view_report(form.get_usage(current_user.city))
-
-
-def view_report(usage_data: List[Usage]):
+    usage_data: List[Usage] = form.get_usage(current_user.city)
     hotels = {usage.hotel for usage in usage_data}
     hotel_counts = [(hotel, sum(1 for usage in usage_data if usage.hotel == hotel)) for hotel in hotels]
     hotel_counts.sort(key=itemgetter(1), reverse=True)
-    return render_template("view_report.html", hotel_counts=hotel_counts, title="Report")
+    return render_template("hotel_count.html", form=form, hotel_counts=hotel_counts, title="Report",
+                           selection=form.get_selection(), total=len(usage_data))
 
 
 @fs_app.route("/profile", methods=["GET", "POST"])
