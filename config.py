f5e17e99062da774072fbd31fa3824ca374ca301
crazynayan <nayan@crazyideas.co.in>
2020-06-22 17:12:14 +0530

    v25 - Today is 31-Dec-2019. Fix for ipad button appearances.

--- config.py
+++ config.py
@@ -66,7 +66,7 @@ class BaseMap:
 
 
 class Date:
-    TODAY = dt.date(2019, 6, 30)
+    TODAY = dt.date(2019, 12, 31)
     INDIA_TIME_ZONE = timezone("Asia/Kolkata")
 
     def __init__(self, date: Union[dt.date, str] = None):
